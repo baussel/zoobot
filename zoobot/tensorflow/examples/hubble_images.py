8a50d5b875b26ad63b68484c0f5b678ca85c270e
Ben Aussel <baussel@yahoo.de>
2022-10-19 20:21:31 +0200

    Added image and label import

--- zoobot/tensorflow/examples/hubble_images.py
+++ zoobot/tensorflow/examples/hubble_images.py
@@ -12,4 +12,22 @@ import tensorflow as tf
 from tensorflow.keras import layers, regularizers
 from tqdm import tqdm
 
+from zoobot.shared import label_metadata, schemas
+from zoobot.tensorflow.data_utils import image_datasets
+from zoobot.tensorflow.estimators import define_model, preprocess, custom_layers
+from zoobot.tensorflow.predictions import predict_on_dataset
+from zoobot.tensorflow.training import training_config, losses
+from zoobot.tensorflow.transfer_learning import utils
+
+#Image parameters
+initial_size = 300 
+crop_size = int(initial_size * 0.75)
+resize_size = 224  #Zoobot, as pretrained, expects 224x224 images
+file_format = "png"
+
+#Batch size
+batch_size = 128
+
+#Read in the dataset
+df = pd.read_csv("/content/drive/MyDrive/MPE/2022_Ben_Aussel/Data/Hubble_COSMOS_labels_complete.csv")
 
